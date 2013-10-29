57fb0b8253c2cb9fe36716e02c6f479919b68d81
Grady Weyenberg <grady.weyenberg@uky.edu>
2013-10-29 13:09:40 -0400

    simplified target density fn

--- R/density.R
+++ R/density.R
@@ -1,6 +1,3 @@
-set.seed(12345)
-
-means <- t(replicate(20, runif(3,-10,10)))
 ##' Trivariate 20-part gaussian mixture.
 ##' @title target density
 ##' @param x matrix with observations in rows
@@ -8,6 +5,10 @@ means <- t(replicate(20, runif(3,-10,10)))
 ##' @return vector of length nrow(x) with density values
 ##' @author Grady Weyenberg
 ##' @export
+##' @examples
+##' means <- t(replicate(20, runif(3,-10,10)))
+##' target(matrix(1:12,4,3),means)
+##' ## rowMeans(apply(means,1,function(mean) dmvnorm(matrix(1:12,4,3),mean,diag(3)/10)))
 target <- function(x,means){
   if(ncol(x) != 3L) stop("x must have 3 colums")
   const <- 3 * (log(0.1)+log(2*pi))
@@ -19,8 +20,8 @@ target <- function(x,means){
   rowMeans(apply(means,1,df))
 }
 
-## rowMeans(apply(means,1,function(mean) dmvnorm(matrix(1:12,4,3),mean,diag(3)/10)))
-target(matrix(1:12,4,3),means)
 
-x <- seq(-10,10,0.05)
-outer(x,x, function(y,z) dnorm(c(y,z,0)))
+
+
+
+
